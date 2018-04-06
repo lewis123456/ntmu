3b2f92d0fe0c5bb63ad1c7ffc71cd0207bf9a05c
lewis <2328811532@qq.com>
2018-04-06 09:58:38 +0800

    add index.html

---
+++ build/resources/main/sql/create_table.sql
@@ -0,0 +1,14 @@
+create table if not exists insert_lock (
+  id int(10) unsigned not null auto_increment,
+  method varchar(128) not null comment '方法名',
+  ip_thread_id varchar(256) not null comment '标识某个线程',
+  create_time timestamp not null default current_timestamp comment '创建时间',
+  update_time timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
+  primary key (id),
+  constraint unique(method)
+)engine=innodb, default charset=utf8, row_format=dynamic;
+
+#insert into insert_lock(method, ip_thread_id) values('addUser', '10.123.12.23_3244');
+#update insert_lock set method = 'updateUser' where method = 'addUser';
+#alter table insert_lock engine=innodb, default charset=utf8, row_format=dynamic;
+alter table insert_lock drop index ip_thread_id;
